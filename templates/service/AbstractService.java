4343d7ab3a20970cb6efb5d4659f41a914649f67
sundaramss <sundaramss@gmail.com>
2012-06-12 14:17:49 +0530

    Controller and service Templates added

---
+++ templates/service/AbstractService.java
@@ -0,0 +1,12 @@
+package ${config.project.packageName}.service;
+
+
+/**
+ *
+ * @author ${config.project.author}
+ */
+
+public class AbstractService {
+
+    
+}
---
+++ templates/service/ServiceLocator.java
@@ -0,0 +1,15 @@
+package ${config.project.packageName}.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 
+ * @author ${config.project.author}
+ */
+@Service
+public class ServiceLocator {
+
+
+}
