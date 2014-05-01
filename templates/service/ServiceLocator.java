125c9982a4d75a5e5fa6370d004401204c0a8db3
sundaramss <sundaramss@gmail.com>
2014-05-01 21:59:20 +0530

    ServiceLocator code correction

--- templates/service/ServiceLocator.java
+++ templates/service/ServiceLocator.java
@@ -11,23 +11,37 @@ import ${config.project.packageName}.${mData.subsystem.toLowerCase()}.service.${
  * 
  * @author ${config.project.author}
  */
-@Service
 public class ServiceLocator {
 
+	private static ServiceLocator instance;
+	
+	private ServiceLocator(){
+	}
+	
+	public static ServiceLocator getInstance(){
+		if(instance == null){
+			instance = new ServiceLocator();
+		}
+		return instance;
+	}
+
 <% config.project.subsystems.each { subsystem -> 
-	def subsystemName = subsystem[0].toUpperCase() + subsystem[1..-1]%>
-	private ${subsystemName}Service  ${subsystem}Service;<%}%>
+	def subsystemName = subsystem[0].toUpperCase() + subsystem[1..-1]
+	def subsystemNameRef = modelHelper.getVariableRef([name:subsystem])%>
+	private ${subsystemName}Service  ${subsystemNameRef}Service;<%}%>
 <% config.project.subsystems.each { subsystem -> 
-	def subsystemName = subsystem[0].toUpperCase() + subsystem[1..-1]%>
+	def subsystemName = subsystem[0].toUpperCase() + subsystem[1..-1]
+	def subsystemNameRef = modelHelper.getVariableRef([name:subsystem])%>
 	@Autowired
-	public void set${subsystemName}Service(${subsystemName}Service  ${subsystem}Service){
-			this.${subsystem}Service = ${subsystem}Service;
+	public void set${subsystemName}Service(${subsystemName}Service  ${subsystemNameRef}Service){
+			this.${subsystemNameRef}Service = ${subsystemNameRef}Service;
 	}
 	<%}%>
 
-	<% modelData.each { mData -> %>
+	<% modelData.each { mData -> 
+	def subsystemNameRef = modelHelper.getVariableRef([name:mData.subsystem])%>
 	public ${mData.className}Service  get${mData.className}Service() { 
-			return ${mData.subsystem}Service;
+			return ${subsystemNameRef}Service;
 	}<%}%>
 
 }
